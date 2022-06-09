63c360d6c5a609a11c3b0de45e8fc995e4e49fa8
vrinda-mahajan <vrindamahajan96@gmail.com>
2022-06-09 18:28:24 +0530

    Added follow btn to profile

--- src/features/profile/profileDetailsCard.jsx
+++ src/features/profile/profileDetailsCard.jsx
@@ -1,18 +1,25 @@
 import { Avatar, Box, Button, Flex, Heading, Link, Text, useDisclosure } from "@chakra-ui/react"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom";
 import { EditProfileModal } from "./editProfileModal";
+import { followUser, unfollowUser } from "./userSlice";
 
 export const ProfileDetailsCard = () => {
+    const dispatch = useDispatch()
     const {isOpen,onOpen,onClose} = useDisclosure()
-    const {allUsers,usersStatus} = useSelector((store) =>store.users)
+    const {user} = useSelector((store) =>store.auth)
+    const {allUsers} = useSelector((store) =>store.users)
     const {allPosts} = useSelector((store) => store.posts)
     const {currUsername} = useParams()
     const selectedUser = allUsers.find(({username})=>username===currUsername)
-    const {firstName,lastName,username,bio,profilePic,portfolio,followers,following} = selectedUser;
+    const {_id,firstName,lastName,username,bio,profilePic,portfolio,followers,following} = selectedUser;
     const userPosts = allPosts.filter(({username})=>username===currUsername)
+    const followHandler = () => {
+        dispatch(followUser(_id));
+    }
     return(
         <Flex mt='50px' align='center' direction='column'>
+            {console.log("Following",selectedUser.following,"followers",selectedUser.followers)}
             <Box p='16px' align='center' w={'350px'} boxShadow='base'>
                 <Avatar mr="10px" name={username} size="2xl" src={profilePic} />
                 <Heading pt='20px' fontSize='20px'>{`${firstName} ${lastName}`}</Heading>
@@ -33,9 +40,18 @@ export const ProfileDetailsCard = () => {
                         <Heading size='16px'>Following</Heading>
                     </Flex>
                 </Flex>
-                <Button mt="20px" width="80px" backgroundColor="primary" color='white' _hover={{bg: 'gray.700'}} onClick={onOpen} >
+                {user.username===username
+                ?<Button mt="20px" width="80px" backgroundColor="primary" color='white' _hover={{bg: 'gray.700'}} onClick={onOpen} >
                     Edit
                 </Button>
+                :(selectedUser && selectedUser?.followers.some(users=>users.username===user.username))?
+                <Button mt="20px" width="100px" backgroundColor="primary" color='white' _hover={{bg: 'gray.700'}} >
+                    Following
+                </Button>
+                :<Button mt="20px" width="100px" backgroundColor="primary" color='white' _hover={{bg: 'gray.700'}} onClick={followHandler} >
+                    Follow
+                </Button>}
+                
             </Box>
            
             <EditProfileModal isOpen={isOpen} onClose={onClose} bio={bio} portfolio={portfolio} />
